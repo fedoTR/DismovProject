e20429e33084d4ec1b48a6f56db38d2359bb5472
sjas666 <numerocuarentayuno@gmail.com>
2021-03-26 01:38:54 -0600

    Register finished screen, can register now, and null pointer exception has comeback.

--- app/src/main/java/com/example/dismovproject/Register.java
+++ app/src/main/java/com/example/dismovproject/Register.java
@@ -1,27 +1,18 @@
 package com.example.dismovproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Text;
 
 import java.util.HashMap;
 import java.util.Map;
