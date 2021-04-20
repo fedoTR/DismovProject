6002abfc74832cc40942d05c7cecdbed74ddcb44
sjas6 <numerocuarentayuno@gmail.com>
2021-04-19 22:54:54 -0500

    Added queries to retrieve data from cloud firebase database on miperfilFragment.java.
    Add a datepicker to Register.java

--- app/src/main/java/com/example/dismovproject/MainActivity.java
+++ app/src/main/java/com/example/dismovproject/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.dismovproject;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
